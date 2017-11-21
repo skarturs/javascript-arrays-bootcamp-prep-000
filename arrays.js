2852c5e94ac4d1da84cdb69d3a035509a0aac9a6
Arturs Teihmanis <arturs.teihmanis@gmail.com>
2017-11-21 20:05:32 +0000

    automatically backed up by learn

---
+++ arrays.js
@@ -0,0 +1,8 @@
+//var ingredient1 = "bread"
+//var ingredients2 = "mild cheese"
+//var ingredient3 = "sharp cheese"
+//var ingredient4 = "butter"
+//var ingredient5 = "tomato"
+//var ingredient6 = "garlic"
+
+var chocolateBars = ["snickers","nudred grand","kitkat","skittles"];
